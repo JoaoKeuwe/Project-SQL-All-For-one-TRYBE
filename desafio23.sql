57d049c8be520e05bd0474e454899406242b7ef4
JoaoKeuwe <joaov2872@hotmail.com>
2022-04-07 13:42:40 -0300

    requisito 23,24

--- desafio23.sql
+++ desafio23.sql
@@ -1,3 +1,3 @@
 UPDATE northwind.order_details
-SET unit_price = 30
+SET discount = 30
 WHERE unit_price < 10.000;
--- desafio24.sql
+++ desafio24.sql
@@ -1,4 +1,4 @@
 UPDATE northwind.order_details
 SET discount = 45
 WHERE unit_price > 10.000
-AND id IN (30,40);
+AND (id	 BETWEEN 30 and 40);
